76254357578bb2e86d2b554a29e1a4995b3ebda2
Aplet123 <aplet@aplet.me>
2023-04-18 10:25:34 -0700

    funny number

---
+++ intro/funny-number/chall.py
@@ -0,0 +1,1 @@
+import collections as c;n=str(int(input("What's the funny number? "))).zfill(10);assert len(n)==10;x=c.Counter(n);print(["Not funny enough!",open("flag.txt","r").read().strip()]["".join(str(x.get(str(i),0))for i in range(10))==n])
