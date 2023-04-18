e338bd9ef99701085051b7e2b67bc9ef84057a0f
Aplet123 <aplet@aplet.me>
2023-04-18 00:39:49 -0700

    minibomb and login-form

---
+++ intro/minibomb/minibomb.c
@@ -0,0 +1,64 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+const char* secret = "pretty pretty pretty pretty pretty pretty please don't blow up\n";
+
+void explode() {
+    puts("BOOM! The bomb exploded. You lost 100% of your grade (it went from 0 to 0).");
+    exit(1);
+}
+
+void defuse() {
+    puts("You defused the bomb!");
+    FILE* flagfile = fopen("flag.txt", "r");
+    if (flagfile == NULL) {
+        puts("Couldn't read flag file. Try connecting to the server to run instead.");
+    } else {
+        char buf[256];
+        fgets(buf, 256, flagfile);
+        buf[strcspn(buf, "\n")] = '\0';
+        puts("Here's the flag:");
+        puts(buf);
+    }
+    exit(0);
+}
+
+int main(void) {
+    setbuf(stdout, NULL);
+    char buf[256];
+    puts("What's the password?");
+    fgets(buf, 256, stdin);
+    if (strcmp(buf, secret) != 0) {
+        explode();
+    }
+    puts("What's my favorite number?");
+    fgets(buf, 256, stdin);
+    long long n = atoll(buf);
+    int v = 10;
+    switch (n) {
+        case 1337133713370LL:
+            v = 0;
+            break;
+        case 1337133713371LL:
+            v = 1;
+        case 1337133713372LL:
+            v += 1;
+        case 1337133713373LL:
+            v *= 2;
+            break;
+        case 1337133713374LL:
+            v = 42;
+            break;
+        case 1337133713375LL:
+            v = 1337;
+            break;
+        default:
+            explode();
+    }
+    if (v != 22) {
+        explode();
+    }
+    defuse();
+    return 0;
+}
